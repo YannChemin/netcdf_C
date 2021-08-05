5d2eff4406474699f66c4622a04eb471f2ab1bdf
YannChemin <yann.chemin@gmail.com>
2021-08-05 13:35:29 +0200

    solved netcdf reading

--- becsmos.c
+++ becsmos.c
@@ -24,33 +24,45 @@ int main( int argc, char *argv[] )
 	}
 	int row,col;
 	float smosx;
+	double scale_factor=9.9999997e-05;
 	//Register all GDAL image drivers to memory
 	GDALAllRegister();
 	//Loading the input files names
 	GDALDatasetH hD[MAXFILES];
 	GDALDatasetH hDQC[MAXFILES];
-	//for ( int x = 1; x < (argc/2); x++ )
-	for ( int x = 1; x < argc; x++ )
+	for ( int x = 0; x < ((argc-1)/2)+1; x++ )
     	{
-		hD[x] = GDALOpen(argv[x],GA_ReadOnly); //Read Band
+		printf("%d/%d : %s\n", x, argc, argv[x]);
+	}
+	for ( int x = ((argc-1)/2)+1; x < argc; x++ )
+    	{
+		printf("%d/%d : %s\n", x, argc, argv[x]);
+	}
+	for ( int x = 1; x < ((argc-1)/2)+1; x++ )
+    	{
+		printf("%d : %s\n", x, argv[x]);
+		hD[x-1] = GDALOpen(argv[x],GA_ReadOnly); //Read Band
 		//Fail the application if unable to load any input file
-		if(hD[x]==NULL){
-			printf("Unable to load one or more input file\n");
+		if(hD[x-1]==NULL){
+			printf("Unable to load input file %s\n",argv[x]);
 			exit(EXIT_FAILURE);
 		}
     	}
-	//for ( int x = (argc/2); x < argc; x++ )
-    	//{
-	//	hDQC[x] = GDALOpen(argv[x],GA_ReadOnly); //Read QC
-	//	//Fail the application if unable to load any input file
-	//	if(hDQC[x]==NULL){
-	//		printf("Unable to load one or more input file\n");
-	//		exit(EXIT_FAILURE);
-	//	}
-    	//}
+	for ( int x = ((argc-1)/2)+1; x < argc; x++ )
+    	{
+		printf("%d : %s\n", x, argv[x]);
+		int xc = x-((argc-1)/2)-1;
+		printf("Counter for hDQC is at %d\n",xc);
+		hDQC[xc] = GDALOpen(argv[x],GA_ReadOnly); //Read QC
+		//Fail the application if unable to load any input file
+		if(hDQC[xc]==NULL){
+			printf("Unable to load input file %s\n",argv[x]);
+			exit(EXIT_FAILURE);
+		}
+    	}
 	//Loading the files drivers
 	GDALDriverH hDr = GDALGetDatasetDriver(hD[0]);
-	//GDALDriverH hDrQC = GDALGetDatasetDriver(hDQC[0]);
+	printf("Passed 1\n");
 
 	//Creating output file
 	double        adfGeoT[6];
@@ -68,42 +80,61 @@ int main( int argc, char *argv[] )
 	    printf( "Origin = (%.6f,%.6f)\n",adfGeoT[0], adfGeoT[3] );
 	    printf( "Pixel Size = (%.6f,%.6f)\n",adfGeoT[1], adfGeoT[5] );
 	}
+
+	//Loading the files drivers
+	GDALDriverH hDrQC = GDALGetDatasetDriver(hDQC[0]);
+	printf("Passed 2\n");
+
+	//Creating output file
+	double        adfGeoTQC[6];
+	printf( "Driver: %s/%s\n",
+	        GDALGetDriverShortName( hDrQC ),
+	        GDALGetDriverLongName( hDrQC ) );
+	printf( "Size is %dx%dx%d\n",
+	        GDALGetRasterXSize( hDQC[0] ),
+	        GDALGetRasterYSize( hDQC[0] ),
+	        GDALGetRasterCount( hDQC[0] ) );
+	if( GDALGetProjectionRef( hDQC[0] ) != NULL )
+	    printf( "Projection is `%s'\n", GDALGetProjectionRef( hDQC[0] ) );
+	if( GDALGetGeoTransform( hDQC[0], adfGeoTQC ) == CE_None )
+	{
+	    printf( "Origin = (%.6f,%.6f)\n",adfGeoTQC[0], adfGeoTQC[3] );
+	    printf( "Pixel Size = (%.6f,%.6f)\n",adfGeoTQC[1], adfGeoTQC[5] );
+	}
+
 	//Defining the file bands and loading them
 	GDALRasterBandH hB[MAXFILES];
 	GDALRasterBandH hBQC[MAXFILES];
-	for ( int x = 1; x < argc; x++ )
+	printf("<><><><><>%d",(argc/2));
+	for ( int x = 0; x < (argc/2); x++ )
     	{
 		hB[x] = GDALGetRasterBand(hD[x],1);
-		//hBQC[x] = GDALGetRasterBand(hDQC[x],1);
+		hBQC[x] = GDALGetRasterBand(hDQC[x],1);
 		//Loading Input file number 1 rows and columns
 		int nX = GDALGetRasterBandXSize(hB[x]);
 		int nY = GDALGetRasterBandYSize(hB[x]);
 		//Preparing processing
 		float *lineB;
-		//SWIR3 Band row
+		//QC Band row
 		int *lineQ;
 		//Defining the data rows
 		lineB = (float *) malloc(sizeof(float)*nX);
 		lineQ = (int *) malloc(sizeof(int)*nX);
 		//Accessing the data rowxrow
-		for(row=0 ; row<nY ; row++){
+		for(row = 0 ; row < nY ; row++){
 			GDALRasterIO(hB[x],GF_Read,0,row,nX,1,lineB,nX,1,GDT_Float32,0,0);
-			//GDALRasterIO(hBQC[x],GF_Read,0,row,nX,1,lineQ,nX,1,GDT_Int32,0,0);
+			GDALRasterIO(hBQC[x],GF_Read,0,row,nX,1,lineQ,nX,1,GDT_Int32,0,0);
 			//Processing the data cellxcell
-			for(col=0 ; col<nX ; col++){
-				//if no data or quality flag fail 
-				//then assign fail values
-				if(lineB[col]==-28768){ /*skip it*/
-					smosx = -28768;
-				}
-				// if pixel are correct, process our algorithms
-				else {
-					//Create Meteo array
-					smosx = lineB[col];
+			for(col = 0 ; col < nX ; col++){
+				//if no data or quality flag fail skip it
+				if(lineB[col]!=-999.0 && lineQ[col]==0){
+					// if pixel are correct, process our algorithms
+					// process the correction to m3/m3
+					smosx = lineB[col] * scale_factor;
+					// Write to meteo array
+					printf("Write to meteo array SM=%0.4f\n", smosx );
 				}
 			}
-			//Write to meteo array
-			printf("Write to meteo array %f\n", smosx );
 		}
 		if( lineB != NULL ) free( lineB );
 		if( lineQ != NULL ) free( lineQ );
